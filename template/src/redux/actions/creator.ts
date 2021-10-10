9872d18312f00ca7099da66f44de56f599029b99
pabloapausa <pabloapausa@outlook.com>
2021-10-10 15:52:52 +0200

    Create actions template

--- template/src/redux/actions/creator.ts
+++ template/src/redux/actions/creator.ts
@@ -1,1 +1,12 @@
+import axios from 'axios';
 import actions from './types';
+
+export default function Creator() {
+  return async (dispatch: any) => {
+    const response = await axios('url');
+    dispatch({
+      type: actions.READ,
+      action: response,
+    });
+  };
+}
